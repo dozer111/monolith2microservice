59b19c3a5d315ad4f0edddbc9755e2fe45b9a893
dozer111 <alkhonko@gmail.com>
2022-03-19 15:42:59 +0200

    init переименовал папки

---
+++ frontend/next-checkout/constants.ts
@@ -0,0 +1,4 @@
+export default {
+    endpoint: 'http://localhost:8000/api/checkout',
+    stripe_key: 'pk_test_51KeylnCqPyerVaeVifSpztRTHSgGXi6G2ag3Pfs5OYSuS3LAV8scjC7pG8kcsXemMaMeG0PHHr8R2LJ13QwkIiZv0027jGVFQh'
+}
---
+++ monolith/README.md
@@ -0,0 +1,1 @@
+mailhog: `~/go/bin/MailHog`
