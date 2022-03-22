5afdffcd038931a81d0c91d4901c03da9b8ca974
dozer111 <alkhonko@gmail.com>
2022-03-22 15:35:19 +0200

    [5 распил admin] перегонка данных

--- admin/database/seeders/DatabaseSeeder.php
+++ admin/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            ProductSeeder::class,
+            LinksSeeder::class,
+            LinkProductsSeeder::class,
+            OrderSeeder::class,
+        ]);
     }
 }
